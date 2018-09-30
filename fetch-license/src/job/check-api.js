ee4a216de823c1c73886c042573dd752c58a6681
michaelrambeau <mikeair@gmail.com>
2018-09-30 16:27:45 +0900

    Add API end-point check

--- fetch-license/src/job/check-api.js
+++ fetch-license/src/job/check-api.js
@@ -4,12 +4,12 @@ const debug = require("debug")("*");
 Check if the API is up and running
 */
 async function checkAPI({ rootUrl }) {
-  throw new Error("bug!");
   const url = `${rootUrl}/status`;
   debug("Checking the API end point", url);
   const response = await got(url, { json: true });
-  debug(response.body);
-  return;
+  const { body } = response;
+  debug(body);
+  return body.status === "OK";
 }
 
 module.exports = checkAPI;
